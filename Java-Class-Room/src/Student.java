b49d2db5914f0de758c0288550452296b7d0cc00
mzujean <101647826+mzujean@users.noreply.github.com>
2022-04-03 15:49:01 +0200

    Student Class- variables
    The ' this.' function was added to the initialized local variables.

--- Java-Class-Room/src/Student.java
+++ Java-Class-Room/src/Student.java
@@ -1,3 +1,5 @@
+
+import java.util.Random;
 public class Student extends Person {
     //Add all student variables with private access modifiers
     private String studentNumber;
@@ -16,15 +18,73 @@ public class Student extends Person {
     
     //Add all constructors
 
-    Student(String name,String surname)
+    public Student(String name,String surname, String studentNumber,
+                String highestSubject, double highestGrade, 
+                    double lowestGrade, String lowestSubject, Subject[] subjects, 
+                        boolean passed)
     {
         super(name, surname);
+      //  this.name = name;
+      //  this.surname = surname;
+        this.studentNumber = studentNumber;
+        this.highestGrade = highestGrade;
+        this.highestSubject = highestSubject;
+        this.lowestGrade = lowestGrade;
+        this.lowestSubject = lowestSubject;
+        this.subjects = subjects;
+        this.passed = passed;
+
+
+
+
+
     }
     //Add all getters and setters for local variables 
 
+    //Student Number
+
+    public String getstudentNumber(){
+        return studentNumber;
+    }
+    //highest grade
+
+    public double gethighestGrade(){
+        return highestGrade;
+    }
+    //highest subject
+
+    public String gethighestSubject(){
+        return highestSubject;
+    }
+    //lowest Grade
+
+    public double getlowestGrade(){
+        return lowestGrade;
+    }
+    //lowest Subject
+
+    public String getlowestSubject(){
+        return lowestSubject;
+    }
+    // subject
+    public Subject subjects(){
+        return subjects();
+    }
     //Add all helper functions for the class
 
+    //random student number generator 
+    
+    public void generateStudentNumber(String studentNumbeString) {}
+        public static void main(String[] args) {
+        	Random objGenerator = new Random();
+            for (int iCount = 0; iCount< 10; iCount++){
+              int randomNumber = objGenerator.nextInt(99999999);
+              System.out.println("Student Number : student - " + randomNumber); 
+            }
+        
+    }
 
+            
     void printReportCard()
     {
         // print out to the terminal the report card of the student
